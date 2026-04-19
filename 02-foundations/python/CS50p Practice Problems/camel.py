37f85a27f77cfc887f05c9e209b54f6ffcff9643
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:12:31 -0400

    Update camel.py

--- 02-foundations/python/CS50p Practice Problems/camel.py
+++ 02-foundations/python/CS50p Practice Problems/camel.py
@@ -1,1 +1,10 @@
+input = input("camelCase: ")
+output = input
 
+for char in input:
+    if char.isupper() == True:
+        output = output.replace(char, f"_{char.lower()}")
+    else:
+        None
+
+print(f"snake_case: {output}")
