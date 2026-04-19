6ff7b202843482268e5ef6d60a18752cf082023b
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:12:09 -0400

    Update bitcoin.py

--- 02-foundations/python/CS50p Practice Problems/bitcoin.py
+++ 02-foundations/python/CS50p Practice Problems/bitcoin.py
@@ -1,1 +1,25 @@
+import sys
+import requests
 
+try:
+    n = float(sys.argv[1])
+except IndexError:
+    sys.exit("Missing command-line argument")
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+try:
+    url = "https://rest.coincap.io/v3/assets/bitcoin?apiKey=9e446636a7188638654577a3f8c6969454f8bd2e5b1c8f0a94d0eb0d59b96061"
+    response = requests.get(url)
+    json_response = response.json()
+
+    btc_price = json_response["data"]["priceUsd"]
+except requests.RequestException:
+    sys.exit()
+
+
+output = float(btc_price) * float(n)
+
+
+
+print(f"${output:,.4f}")
