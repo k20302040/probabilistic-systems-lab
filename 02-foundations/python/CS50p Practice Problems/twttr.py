2af370cbd1a97d90be6066183de9b2e3882cdc08
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:18:07 -0400

    Update twttr.py

--- 02-foundations/python/CS50p Practice Problems/twttr.py
+++ 02-foundations/python/CS50p Practice Problems/twttr.py
@@ -1,1 +1,10 @@
+input = input("Input: ")
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+output = input
 
+for char in input:
+    if char in vowels:
+        output = output.replace(char, "")
+
+
+print(f"output: {output}")
