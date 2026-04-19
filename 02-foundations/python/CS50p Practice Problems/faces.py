3fcbeee42b437ab84255258caf6792b794008c75
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:14:16 -0400

    Update faces.py

--- 02-foundations/python/CS50p Practice Problems/faces.py
+++ 02-foundations/python/CS50p Practice Problems/faces.py
@@ -1,1 +1,16 @@
+emoji_dict = {
+    ":)" : "🙂",
+    ":(" : "🙁"
+}
+user_input = input("")
+user_input_list = user_input.split(" ")
 
+for item in user_input_list:
+    if item not in emoji_dict:
+        pass
+    if item in emoji_dict:
+        item_index = user_input_list.index(item)
+        user_input_list[item_index] = emoji_dict[item]
+
+output = ' '.join(user_input_list)
+print(output, end = "")
