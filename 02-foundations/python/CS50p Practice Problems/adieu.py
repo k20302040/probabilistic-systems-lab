e4bd74bd78dfb3924cfb55f82742928958fe97fa
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:09:08 -0400

    Update adieu.py

--- 02-foundations/python/CS50p Practice Problems/adieu.py
+++ 02-foundations/python/CS50p Practice Problems/adieu.py
@@ -1,1 +1,14 @@
+import inflect
+inflct = inflect.engine()
 
+names = []
+
+while True:
+    try:
+        new_name = input("Name: ")
+        names.append(new_name)
+    except EOFError:
+        print()
+        output = inflct.join(names)
+        print("Adieu, adieu, to" + " " + output)
+        raise SystemExit
