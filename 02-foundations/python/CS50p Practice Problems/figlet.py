2845642cd1a96feee31753f8fc7424e9e22b35e0
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:14:29 -0400

    Update figlet.py

--- 02-foundations/python/CS50p Practice Problems/figlet.py
+++ 02-foundations/python/CS50p Practice Problems/figlet.py
@@ -1,1 +1,21 @@
+import sys
+import random
+import pyfiglet
 
+
+if len(sys.argv) == 1:
+    input = input("Input: ")
+    f = pyfiglet.figlet_format(input, font = random.choice(pyfiglet.FigletFont.getFonts()))
+    print(f)
+elif len(sys.argv) == 2:
+    sys.exit("Invalid usage")
+elif len(sys.argv) == 3:
+    if sys.argv[1] == "-f" or sys.argv[2] == "--font":
+        input = input("Input: ")
+        font = sys.argv[2]
+        f = pyfiglet.figlet_format(input, font = font)
+        print(f)
+    else:
+        sys.exit("Invalid usage")
+else:
+    sys.exit("Invalid usage")
