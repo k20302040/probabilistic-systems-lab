2b9f182f010f326289f5ed2d63bad7d08d1ccbf1
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:15:49 -0400

    Update interpreter.py

--- 02-foundations/python/CS50p Practice Problems/interpreter.py
+++ 02-foundations/python/CS50p Practice Problems/interpreter.py
@@ -1,1 +1,16 @@
+x , y , z = input("Expression: ").split()
 
+# Change type of variable x and z
+x = float(x)
+z = float(z)
+
+# Perform the operations
+match y:
+    case "+":
+        print(x + z)
+    case "-":
+        print(x - z)
+    case "/":
+        print(x / z)
+    case "*":
+        print(x * z)
