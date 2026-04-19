dfb5831c4370494af6401eb863d612e5952e209c
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:16:51 -0400

    Update plates.py

--- 02-foundations/python/CS50p Practice Problems/plates.py
+++ 02-foundations/python/CS50p Practice Problems/plates.py
@@ -1,1 +1,43 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
+
+def is_valid(plate):
+
+    # Length check
+    if len(plate) < 2 or len(plate) > 6:
+        return False
+
+    # First two must be letters
+    if not plate[0].isalpha() or not plate[1].isalpha():
+        return False
+
+    # Only letters and numbers
+    if not plate.isalnum():
+        return False
+
+    # Numbers rules
+    number_started = False
+    for char in plate:
+        if char.isdigit():
+            if not number_started:
+                # first number cannot be 0
+                if char == "0":
+                    return False
+                number_started = True
+        else:
+            # letter after number = invalid
+            if number_started:
+                return False
+
+    return True
+
+
+
+
+
+main()
