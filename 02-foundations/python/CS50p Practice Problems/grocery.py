acd51eb94082287a888f5c4bff5ea96cabdad666
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:15:13 -0400

    Update grocery.py

--- 02-foundations/python/CS50p Practice Problems/grocery.py
+++ 02-foundations/python/CS50p Practice Problems/grocery.py
@@ -1,1 +1,18 @@
+grocery_list = {}
 
+while True:
+    try:
+        item = input()
+        item = item.upper()
+        if item in grocery_list:
+            grocery_list[item] = grocery_list[item] + 1
+        else:
+            grocery_list[item] = 1
+
+    except EOFError:
+        sorted_grocery_list = dict(sorted(grocery_list.items()))
+        for key, value in sorted_grocery_list.items():
+            print(f"{value} {key}")
+
+
+        break
