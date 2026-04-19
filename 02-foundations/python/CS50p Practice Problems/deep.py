82f351214efacbebefc684f7adba92eaff8d82ed
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:13:03 -0400

    Update deep.py

--- 02-foundations/python/CS50p Practice Problems/deep.py
+++ 02-foundations/python/CS50p Practice Problems/deep.py
@@ -1,1 +1,6 @@
+input = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
 
+if "42" in input or ("four" and "two" in input):
+    print("Yes")
+else:
+    print("No")
