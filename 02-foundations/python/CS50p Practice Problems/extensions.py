e48f6ee19db6ca7044e56c3bd5bc986df1b5f4d5
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:13:59 -0400

    Update extensions.py

--- 02-foundations/python/CS50p Practice Problems/extensions.py
+++ 02-foundations/python/CS50p Practice Problems/extensions.py
@@ -1,1 +1,16 @@
+input = input("Enter file: ").lower()
 
+if ".gif" in input:
+    print("image/gif")
+elif ".jpg" in input or ".jpeg" in input:
+    print("image/jpeg")
+elif ".png" in input:
+    print("image/png")
+elif ".pdf" in input:
+    print("application/pdf")
+elif ".txt" in input:
+    print("text/plain")
+elif ".zip" in input:
+    print("application/zip")
+else:
+    print("application/octet-stream")
