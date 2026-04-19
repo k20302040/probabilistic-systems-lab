3fa87eb515deab257d03ebde2104bf0cac3669b8
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:14:57 -0400

    Update game.py

--- 02-foundations/python/CS50p Practice Problems/game.py
+++ 02-foundations/python/CS50p Practice Problems/game.py
@@ -1,1 +1,37 @@
+import random
 
+def main(level, randint):
+    # ensure user's range fits reqs
+    while True:
+        if level > 0:
+            break
+        else:
+            level = int(input("Level: "))
+
+    # user's guess
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            break
+        except ValueError:
+            None
+
+    # check
+    if guess <= 0:
+        main(level, randint)
+    elif guess == randint:
+        print("Just right!")
+        raise SystemExit
+    elif guess < randint:
+        print("Too small!")
+        print()
+        main(level, randint)
+    elif guess > randint:
+        print("Too large!")
+        print()
+        main(level, randint)
+
+
+level = int(input("Level: "))
+randint = random.randint(1, level)
+main(level, randint)
