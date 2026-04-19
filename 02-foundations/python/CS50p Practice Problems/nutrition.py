3504b914c8b210d3f2433b4c933794c2f7972245
Krish R <krishrathi2010@gmail.com>
2026-04-19 18:16:17 -0400

    Update nutrition.py

--- 02-foundations/python/CS50p Practice Problems/nutrition.py
+++ 02-foundations/python/CS50p Practice Problems/nutrition.py
@@ -1,1 +1,43 @@
+fruit = input("Item: ")
 
+match fruit:
+    case "apple":
+        print("Calories: 130")
+    case "Avocado":
+        print("Calories: 50")
+    case "Banana":
+        print("Calories: 110")
+    case "Cantaloupe":
+        print("Calories: 50")
+    case "Grapefruit":
+        print("Calories: 60")
+    case "Grapes":
+        print("Calories: 90")
+    case "Honeydew Melon":
+        print("Calories: 50")
+    case "Kiwifruit":
+        print("Calories: 90")
+    case "Lemon":
+        print("Calories: 15")
+    case "Lime":
+        print("Calories: 20")
+    case "Nectarine":
+        print("Calories: 60")
+    case "Orange":
+        print("Calories: 80")
+    case "Peach":
+        print("Calories: 60")
+    case "pear":
+        print("Calories: 100")
+    case "Pineapple":
+        print("Calories: 50")
+    case "Plums":
+        print("Calories: 70")
+    case "Strawberries":
+        print("Calories: 50")
+    case "Sweet Cherries":
+        print("Calories: 100")
+    case "Tangerine":
+        print("Calories: 50")
+    case "Watermelon":
+        print("Calories: 80")
