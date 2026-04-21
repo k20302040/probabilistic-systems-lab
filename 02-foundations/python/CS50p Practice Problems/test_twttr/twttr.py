9a579c7daae113b6cc4d1f547194e44b1405c3f5
Krish R <krishrathi2010@gmail.com>
2026-04-21 16:53:11 -0400

    Update twttr.py

--- 02-foundations/python/CS50p Practice Problems/test_twttr/twttr.py
+++ 02-foundations/python/CS50p Practice Problems/test_twttr/twttr.py
@@ -1,1 +1,23 @@
+import sys
+
+def main():
+    user_input = input("Input: ")
+    if user_input.isdigit() == True:
+        sys.exit()
+    output = f"output: {shorten(user_input)}"
+    print(output)
+
+def shorten(user_input):
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    output = user_input
+
+    for char in user_input:
+        if char in vowels:
+            output = output.replace(char, "")
+    return output
+
+
+
+if __name__ == "__main__":
+    main()
 
